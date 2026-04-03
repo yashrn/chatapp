f901c1d634dc9447e2759c2bd96f4b977c00c4ba
yashrn <yramnani40@gmail.com>
2026-04-03 10:16:35 +0530

    sidebar completed

---
+++ frontend/src/components/NoConversationPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoConversationPlaceHolder() {
+  return (
+    <div>
+      no conversation
+    </div>
+  )
+}
+
+export default NoConversationPlaceHolder
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -62,5 +62,17 @@ export const useAuthStore = create((set) => ({
             toast.error("Error logging out");
             console.log("Logout error",error);
         }
+    },
+
+    updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response.data.message);
     }
+  },
+
 }));
